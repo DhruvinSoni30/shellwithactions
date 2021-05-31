eb644d6c72926ba5ad07e28448aaf553345f7abd
DhruvinSoni30 <dhruvinsoni30@gmail.com>
2021-06-01 03:22:22 +0530

    Modified the code

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,1 +1,15 @@
-echo "Hello World"
+
+function generate_number(){
+    if [ -z "$1" ]
+    then
+        Total_Numbers=10
+    else
+        Total_Numbers=$1
+    fi
+    for ((i = 0; i < ${Total_Numbers}; i++)); do
+    echo $i,$RANDOM >> $file_location
+    done
+}
+
+check_existance_of_file
+generate_number $1
