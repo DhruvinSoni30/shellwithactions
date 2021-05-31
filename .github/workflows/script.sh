a872ff7f44e3be15f61648a8d6dbd237b41ffe33
Dhruvin Soni <37222087+DhruvinSoni30@users.noreply.github.com>
2021-06-01 03:28:12 +0530

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,14 +1,1 @@
-#bin/bash
-function generate_number(){ 
-    if [ -z "$1" ]
-    then
-        Total_Numbers=10
-    else
-        Total_Numbers=$1
-    fi
-    for (i = 0; i < ${Total_Numbers}; i++); do
-    echo $i,$RANDOM >> number.txt
-    cat number.txt
-    done
-}
-generate_number $1
+echo "Hello World"
