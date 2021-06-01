9a61b08722272f1c601c013f6f69c2089552cc52
DhruvinSoni30 <dhruvinsoni30@gmail.com>
2021-06-01 12:48:41 +0530

    Added new code

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,1 +1,16 @@
-echo "Hello World"
+#binn/bash
+
+function generate_number(){
+ 
+    if [ -z "$1" ]
+    then
+        Total_Numbers=10
+    else
+        Total_Numbers=$1
+    fi
+    for ((i = 0; i < ${Total_Numbers}; i++)); do
+    echo $i,$RANDOM >> number.txt
+    done
+}
+
+generate_number $1
