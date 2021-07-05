d0b7ba675b1171b52bbd73057f51ed75438c5656
Dhruvin Soni <37222087+DhruvinSoni30@users.noreply.github.com>
2021-07-05 23:53:55 +0530

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,1 +1,26 @@
-echo "Hello World"
+# Updating Repository
+apt-get update -y
+
+# Checking disk space
+df -h
+
+# Checking all processes
+ps
+
+# Checking all the port
+netstat -nptl
+
+# Installing Git
+apt-get install git
+
+# Checking Git version
+git --version
+
+# Checking python version
+python --version
+
+# Checking Hostname
+hostname -f
+
+# Checking date & time
+date
